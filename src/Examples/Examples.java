8ac70206ea4a422134ff5f0ec71144ca9645ee46
kenshinwow1 <kenshinwow1@gmail.com>
2023-09-02 12:19:41 +0300

    Exception

---
+++ src/Examples/Examples.java
@@ -0,0 +1,22 @@
+package Examples;
+
+
+public class Examples {
+
+
+    public static void main(String[] args) {
+        int arr[] = {23, 6, 7};
+        try{
+            System.out.println(arr[3]);
+        int x = 100 / 0;
+
+    }       catch(ArithmeticException e) {
+            System.out.println("Error" + e);
+        }catch(Exception e) {
+            System.out.println("Error" + e);
+        }finally {
+            System.out.println("Finally is working");
+            }
+        }
+
+    }
